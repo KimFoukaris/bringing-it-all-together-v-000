b48290a39b41c09eafaae41e8573bb765e1305ca
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-21 07:22:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,4 +66,10 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+    dog_data = dog[0]
+    dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  end
+
 end
