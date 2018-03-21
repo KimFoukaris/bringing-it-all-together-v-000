8d22dd57c66973519abc7f9e5c0d18c9357963a3
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-21 07:27:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,4 +72,9 @@ class Dog
     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end
 
+  def update
+    sql = "UPDATE songs SET name = ?, breed = ?, where id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+    
 end
