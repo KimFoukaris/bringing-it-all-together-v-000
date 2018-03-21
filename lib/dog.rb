805184c27394efd5f396a6e0e2376810e015664f
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-21 06:48:24 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,42 @@
+class Dog
+
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs(name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = self.new(name, breed)
+    dog.save
+    dog
+  end
+
+end
